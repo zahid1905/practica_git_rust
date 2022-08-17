5808a906aa3507f2c01f19c6f906f7479056b4c5
zahid1905 <superpichi.chido@gmail.com>
2022-08-17 08:16:46 -0500

    Changes to be committed: 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 use std::io;
 
 fn main() {
-    let mut input: String = String::new();
 
-    println!("Ingreseun valor int: ");
-
-    io::stdin().read_line(&mut input).expect("No se pudo leer");
-
-    let valor_int: i32 = input.trim().parse().unwrap();
-
-    println!("Valor int: {}", valor_int);
 }
