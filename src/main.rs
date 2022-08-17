61a3a1e3d962fe3358909f2386327bfa9c2681d6
zahid1905 <superpichi.chido@gmail.com>
2022-08-17 07:57:15 -0500

    Changes to be committed: 	modified:   src/main.rs - Leer float (f64) en lugar de int (i32)

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::io;
 fn main() {
     let mut input: String = String::new();
 
-    println!("Ingrese un valor int: ");
+    println!("Ingrese un valor double: ");
 
     io::stdin().read_line(&mut input).expect("No se pudo leer");
 
-    let valor_int: i32 = input.trim().parse().unwrap();
+    let valor_double: f64 = input.trim().parse().unwrap();
 
-    println!("Valor int: {}", valor_int);
+    println!("Valor double: {}", valor_double);
 }
