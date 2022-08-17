c6053d25d30ddaaa67dba77a7b86292436658713
zahid1905 <superpichi.chido@gmail.com>
2022-08-17 08:06:43 -0500

    Changes to be committed: 	modified:   src/main.rs - Reducido el codigo de rama master

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use std::io;
-
 fn main() {
-    let mut input: String = String::new();
-
-    println!("Ingrese un valor int: ");
-
-    io::stdin().read_line(&mut input).expect("No se pudo leer");
-
-    let valor_int: i32 = input.trim().parse().unwrap();
-
-    println!("Valor int: {}", valor_int);
+    println!("Ingresar e imprimir valores");
 }
