248db773c8a887408ac23199acc5f5145df324dd
zahid1905 <superpichi.chido@gmail.com>
2022-08-17 08:04:00 -0500

    Changes to be committed: 	modified:   src/main.rs - Leer string (String) en lugar de int (i32)

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::io;
 fn main() {
     let mut input: String = String::new();
 
-    println!("Ingrese un valor int: ");
+    println!("Ingrese un valor string: ");
 
     io::stdin().read_line(&mut input).expect("No se pudo leer");
 
-    let valor_int: i32 = input.trim().parse().unwrap();
+    let valor_string: String = input.trim().parse().unwrap();
 
-    println!("Valor int: {}", valor_int);
+    println!("Valor string: {}", valor_string);
 }
