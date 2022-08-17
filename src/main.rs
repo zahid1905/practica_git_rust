3d6ae92bba8078e7ee1077f6dc4c4430aaebefc2
zahid1905 <superpichi.chido@gmail.com>
2022-08-17 07:48:28 -0500

    On branch rama_v.2.1  Changes to be committed: 	modified:   src/main.rs - Leer byte (u8) en lugar de int (i32)

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::io;
 fn main() {
     let mut input: String = String::new();
 
-    println!("Ingrese un valor int: ");
+    println!("Ingrese un valor byte: ");
 
     io::stdin().read_line(&mut input).expect("No se pudo leer");
 
-    let valor_int: i32 = input.trim().parse().unwrap();
+    let valor_byte: u8 = input.trim().parse().unwrap();
 
-    println!("Valor int: {}", valor_int);
+    println!("Valor int: {}", valor_byte);
 }
